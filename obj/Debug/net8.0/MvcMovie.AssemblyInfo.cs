1546dab819b4e50a7bc92ddbf948235812c8bf60
daAus10 <w0764215@myscc.ca>
2025-02-05 09:14:23 -0500

    about to publish to azure

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a92c259ba66f43765517136d7aa007e3c788af0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76376846d395506cb09feaa67da0184080c09918")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
